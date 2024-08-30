569e969202722d5a04f66ac6b71e7e56b6540731
lewin349 <lewinl349@gmail.com>
2024-08-30 00:28:22 -0400

    Add hover effect for copy button in contact section

--- scripts/script.js
+++ scripts/script.js
@@ -1,4 +1,11 @@
 function copy() {
     navigator.clipboard.writeText("lewinl349@gmail.com");
+
+    var tooltip = document.getElementById("copy-tooltip");
+    tooltip.innerHTML = "Copied!"
 }
 
+function mouseOut() {
+    var tooltip = document.getElementById("copy-tooltip");
+    tooltip.innerHTML = "Copy Email?"
+}
