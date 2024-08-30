a2a93b5037825fa6a4cb40d3aaf74e6430db674d
lewinl349 <lewinl349@gmail.com>
2024-08-30 14:32:26 -0400

    Add background color animation when scrolling

--- scripts/script.js
+++ scripts/script.js
@@ -1,11 +1,31 @@
+document.addEventListener("scroll", () => {
+    changeBackground(window.scrollY);
+});
+
+function rgb(r, g , b) {
+    r = Math.floor(r);
+    g = Math.floor(g);
+    b = Math.floor(b);
+
+    return ["rgb(",r,",",g,",",b,")"].join("");
+}
+
+function changeBackground(scrollPos) {
+    let val = scrollPos / 25;
+    let body = document.body;
+    let color = rgb(24, 24, 38 + val);
+
+    body.style.backgroundColor = color;
+}
+
 function copy() {
     navigator.clipboard.writeText("lewinl349@gmail.com");
 
-    var tooltip = document.getElementById("copy-tooltip");
-    tooltip.innerHTML = "Copied!"
+    let tooltip = document.getElementById("copy-tooltip");
+    tooltip.innerHTML = "Copied!";
 }
 
 function mouseOut() {
-    var tooltip = document.getElementById("copy-tooltip");
-    tooltip.innerHTML = "Copy Email?"
+    let tooltip = document.getElementById("copy-tooltip");
+    tooltip.innerHTML = "Copy Email?";
 }
